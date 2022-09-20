70831ed1f53ce0e313c0e4ddd9d38c5453d0131c
seco0 <107729787+seco0@users.noreply.github.com>
2022-09-21 01:37:20 +0300

    Add files via upload

---
+++ Degiskenler.java
@@ -0,0 +1,16 @@
+package variabless;
+
+public class Degiskenler {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Ogrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj +ogrenciSayisi);
+		System.out.println("Ogrenci sayim : "+ogrenciSayisi);
+		System.out.println("Ogrenci sayim : "+ogrenciSayisi);
+		System.out.println("Ogrenci sayim : "+ogrenciSayisi);
+
+	}
+
+}
