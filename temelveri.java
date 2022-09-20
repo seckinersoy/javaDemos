1491ec1ea0f6e42f71af255ffb014325af6608a1
seco0 <107729787+seco0@users.noreply.github.com>
2022-09-21 02:04:04 +0300

    Add files via upload

---
+++ temelveri.java
@@ -0,0 +1,20 @@
+package datatypes;
+
+public class temelveri {
+
+	public static void main(String[] args) {
+		int sayi =12;
+		
+		
+		
+		char karakter = 'A';
+		
+		boolean dogruMu=false;
+		
+		
+				
+		
+
+	}
+
+}
