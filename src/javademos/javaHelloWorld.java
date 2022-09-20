413395fa54874999d5c176b4bf60c5fb99862d00
secki <secki@DESKTOP-BNAFGN3>
2022-09-21 00:44:07 +0300

    First Commit

---
+++ src/javademos/javaHelloWorld.java
@@ -0,0 +1,11 @@
+package javademos;
+
+public class javaHelloWorld {
+
+	public static void main(String[] args) {
+		
+       System.out.println("Merhaba Java");
+       System.out.println("Merhaba Java 2");
+	}
+
+}
