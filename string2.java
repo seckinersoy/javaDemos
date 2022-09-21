165102019b06be527eb28e1869c4421337e55ac5
seco0 <107729787+seco0@users.noreply.github.com>
2022-09-21 17:03:42 +0300

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,26 @@
+
+public class string2 {
+
+	public static void main(String[] args) {
+		String mesaj= "Bugun hava cok guzel.";
+		System.out.println(mesaj);
+		
+		String yeniMesaj =mesaj.replace(' ', '-');
+		System.out.println(yeniMesaj);
+		System.out.println(mesaj.substring(2,5));
+		
+		for (String kelime : mesaj.split(" ")) {
+			System.out.println(kelime);
+			
+			
+		}
+		
+		System.out.println(mesaj.toLowerCase());
+		System.out.println(mesaj.toUpperCase());
+		System.out.println(mesaj.trim());
+		
+		
+
+	}
+
+}
