50290c2d087278a6f30524d0dc14cbcae50df7ce
seco0 <107729787+seco0@users.noreply.github.com>
2022-09-21 03:44:37 +0300

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,17 @@
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int j=1;
+		do {
+			System.out.println(j);
+			j+=2;
+		}while(j<10);
+		System.out.println("Do While Dongusu Bitti");
+		
+			
+		
+
+	}
+
+}
