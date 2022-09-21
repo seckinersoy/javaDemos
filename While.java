38b9d316de26df1b8fb682b402527713690b31d1
seco0 <107729787+seco0@users.noreply.github.com>
2022-09-21 03:34:25 +0300

    Add files via upload

---
+++ While.java
@@ -0,0 +1,15 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		int i=1;
+		while (i<10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println("While Dongusu Bitti");
+
+	}
+
+}
