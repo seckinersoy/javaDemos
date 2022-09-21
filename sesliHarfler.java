b208686d0151e124b9c127f22554c9624ac1916e
seco0 <107729787+seco0@users.noreply.github.com>
2022-09-21 17:40:22 +0300

    Add files via upload

---
+++ sesliHarfler.java
@@ -0,0 +1,21 @@
+
+public class sesliHarfler {
+
+	public static void main(String[] args) {
+		char harf = 'I';
+
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalin sesli harf");
+			break;
+		default:
+			System.out.println("Ince sesli harf");
+
+		}
+
+	}
+
+}
