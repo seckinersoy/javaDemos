1eeab3c4ca14bc8dd3ce925b85ad32feed6bb1d7
seco0 <107729787+seco0@users.noreply.github.com>
2022-10-04 19:50:53 +0300

    Add files via upload

--- CustomerManager.java
+++ CustomerManager.java
@@ -1,6 +1,12 @@
-package inheritance;
-
-public class CustomerManager extends PersonManager {
-   
- 
+public class CustomerManager {
+   public void Add() {
+	   System.out.println("Musteri eklendi");
+   } 
+   public void Remove() {
+	   System.out.println("Musteri silind");
+   } 
+   public void Update() {
+	   System.out.println("Musteri guncellendi");
+   } 
+  
 }
