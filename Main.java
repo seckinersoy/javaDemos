a6ddfd9c4b49e51c83387dcdce9d48cfc4182ec4
seco0 <107729787+seco0@users.noreply.github.com>
2022-10-04 19:49:44 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,18 @@
-package classesWithAttributes;
-
+package inheritance;
 
 public class Main {
-	 public static void main(String[] args) {
-	        Product product = new Product();
-	        product.setId(1);
-	        product.setName("Laptop");
-	        product.setDescription("Asus Laptop");
-	        product.setPrice(5000);
-	        product.setStockAmount(3);
-	        ProductManager productManager = new ProductManager();
-	        productManager.add(product);
 
-	        System.out.println(product.getCode());
+	public static void main(String[] args) {
+		Customer customer=new Customer();
+		Employee employee=new Employee ();
+		
+		
+		EmployeeManager employeeManager=new EmployeeManager();
+		CustomerManager customerManager=new CustomerManager();
+		
+		
+		
 
-	        Product product2 = new Product(2, "Mouse", "Asus Mouse", 250, 5);
-	        productManager.add(product2);
-	    }
-	
+	}
+
 }
-
-	
-	
