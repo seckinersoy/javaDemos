0a6f1394c51225a403c071b6c4bf10cca9f121b8
seco0 <107729787+seco0@users.noreply.github.com>
2022-10-04 19:50:30 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
-package inheritanceDemo;
+package methodOverloading;
 
 public class Main {
 
 	public static void main(String[] args) {
-		KrediUI krediUI=new KrediUI();
-		krediUI.KrediHesapla(new OgretmenKrediManager());
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.topla(2, 3) ) ;
+		dortIslem.topla(2, 3, 5);
 
 	}
 
